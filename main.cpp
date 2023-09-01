dfd849fd322c50011601982eafe3bdd0e968e49b
Bielefeldt <philipp.bielefeldt@4soft.de>
2023-09-01 13:42:51 +0200

    add second tab

--- main.cpp
+++ main.cpp
@@ -3,39 +3,50 @@
 #include <QTextStream>
 #include <QPlainTextEdit>
 #include <QWidget>
+#include <QTabWidget>
 
-int main(int argc, char *argv[])
+// Function to read and display the contents of a file in a QPlainTextEdit
+void readFileAndDisplay(QTabWidget& tabWidget, const QString& filename, const QString& tabTitle)
 {
-    QApplication app(argc, argv);
-
-    // Create a QWidget (window)
-    QWidget window;
-    window.setWindowTitle("Text File Viewer");
-    window.setGeometry(100, 100, 400, 300);
-
-    // Create a QPlainTextEdit to display the file contents
-    QPlainTextEdit textEdit(&window);
-    textEdit.setGeometry(10, 10, 380, 280);
-
-    // Read the contents of the text file
-    QFile file("../test_1.txt"); // Adjust the file path as needed
+    QFile file(filename);
     if (file.open(QIODevice::ReadOnly | QIODevice::Text))
     {
         QTextStream in(&file);
         QString fileContents = in.readAll();
 
-        // Set the file contents in the QPlainTextEdit
-        textEdit.setPlainText(fileContents);
+        QPlainTextEdit* textEdit = new QPlainTextEdit(&tabWidget);
+        textEdit->setPlainText(fileContents);
+
+        tabWidget.addTab(textEdit, tabTitle);
 
         file.close();
     }
     else
     {
-        // Display an error message if the file couldn't be opened
-        textEdit.setPlainText("Error: Could not open the file.");
-    }
+        QPlainTextEdit* errorTextEdit = new QPlainTextEdit(&tabWidget);
+        errorTextEdit->setPlainText("Error: Could not open " + filename + ".");
 
-    // Show the window
+        tabWidget.addTab(errorTextEdit, tabTitle + " (Error)");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+
+    QWidget window;
+    window.setWindowTitle("Text File Viewer");
+    window.setGeometry(100, 100, 400, 300);
+
+    QTabWidget tabWidget(&window);
+    tabWidget.setGeometry(10, 10, 380, 280);
+
+    // Read and display the contents of test_1.txt
+    readFileAndDisplay(tabWidget, "../test_1.txt", "Test 1");
+
+    // Read and display the contents of test_2.txt
+    readFileAndDisplay(tabWidget, "../test_2.txt", "Test 2");
+
     window.show();
 
     return app.exec();
